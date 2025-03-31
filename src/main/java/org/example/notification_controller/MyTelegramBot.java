8944b297ad1821383e635851c82a61a3095e0c55
IhorZima <ihorrzima@gmail.com>
2025-03-31 23:28:24 +0300

    init

---
+++ src/main/java/org/example/notification_controller/MyTelegramBot.java
@@ -0,0 +1,42 @@
+package org.example.notification_controller;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class MyTelegramBot extends TelegramLongPollingBot {
+
+    public MyTelegramBot(String botToken) {
+        super(botToken);
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "@NotificationSender001bot";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String chatId = update.getMessage().getChatId().toString();
+            String receivedText = update.getMessage().getText();
+
+            if (receivedText.equals("/start")) {
+                sendTextMessage(chatId, "hello it's your Telegram bot 🚀");
+            }
+        }
+    }
+
+    private void sendTextMessage(String chatId, String text) {
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId);
+        message.setText(text);
+
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
